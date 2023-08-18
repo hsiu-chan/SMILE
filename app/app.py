abb172266626721e0d3cd1fbd57f02b5ebec5933
chc <106435999+hsiu-chan@users.noreply.github.com>
2023-08-19 02:44:00 +0800

    changed 13 files

--- app/app.py
+++ app/app.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from api.func1 import func1_blueprint
-from api.img import img_blueprint
-from api.GetLabelData import GetLabelData_blueprint
+#from api.img import img_blueprint
+from api.GetLabelData import GetLabelData_blueprint#取得標注資料
 from website import website_home_blueprint,website_pages_blueprint
 #from flask_mail import Mail, Message
 
@@ -15,9 +15,9 @@ def create_app():#Application Factories
     app.register_blueprint(website_pages_blueprint)
 
     ###API###
-    app.register_blueprint(img_blueprint)
+    #app.register_blueprint(img_blueprint)
     app.register_blueprint(func1_blueprint)
-    app.register_blueprint(GetLabelData_blueprint)
+    app.register_blueprint(GetLabelData_blueprint)#取得標注資料
     
     app.config['MAIL_SERVER']='smtp.gmail.com'
     app.config['MAIL_PORT'] = 465
